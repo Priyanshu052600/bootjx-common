5317008305645628d3d7c456dd6d86b479f8e685
prashantnitdgp@gmail.com <prashantnitdgp@gmail.com>
2023-06-07 16:38:56 +0530

    controller advice

---
+++ bootjx-common-lib/src/main/java/com/boot/jx/web/ErrorResponseAdvice.java
@@ -0,0 +1,10 @@
+package com.boot.jx.web;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import com.boot.jx.exception.AmxAdvice;
+
+@ControllerAdvice
+public class ErrorResponseAdvice extends AmxAdvice{
+	
+}
