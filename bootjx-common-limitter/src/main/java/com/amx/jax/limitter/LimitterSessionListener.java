32091f84cd6e2bcda776a48e0438d41cdd0d91ab
prashantnitdgp@gmail.com <prashantnitdgp@gmail.com>
2024-03-26 18:04:04 +0530

    limitter excp. removal from offline service

--- bootjx-common-limitter/src/main/java/com/amx/jax/limitter/LimitterSessionListener.java
+++ bootjx-common-limitter/src/main/java/com/amx/jax/limitter/LimitterSessionListener.java
@@ -1,12 +1,14 @@
 package com.amx.jax.limitter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import jakarta.servlet.http.HttpSessionEvent;
 import jakarta.servlet.http.HttpSessionListener;
 
 @Service
+@ConditionalOnProperty("app.ratelimit.refreshtoken.enable")
 public class LimitterSessionListener implements HttpSessionListener {
 
     @Autowired
