8efa6c629ef57fd37629c7f0e21b26ef8fe7c542
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-05-04 16:43:31 +0530

    time period

--- bootjx-common-lib/src/main/java/com/boot/jx/AppMVConfig.java
+++ bootjx-common-lib/src/main/java/com/boot/jx/AppMVConfig.java
@@ -1,6 +1,7 @@
 package com.boot.jx;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -11,32 +12,40 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import com.boot.jx.convertor.StringToEnumIgnoringCaseConverterFactory;
 import com.boot.jx.convertor.StringToTimePeriod;
 import com.boot.jx.filter.AppRequestInterceptor;
+import com.boot.utils.TimeUtils;
+
+import org.springframework.core.convert.converter.Converter;
 
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class AppMVConfig extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private AppRequestInterceptor appRequestInterceptor;
-    /** Pattern relative to templates base used to match text templates. */
-    public static final String TEXT_TEMPLATES_RESOLVE_PATTERN = "text/*";
-    /** Pattern relative to templates base used to match JSON templates. */
-    public static final String JSON_TEMPLATES_RESOLVE_PATTERN = "json/*";
-    /** Pattern relative to templates base used to match JSON templates. */
-    public static final String JS_TEMPLATES_RESOLVE_PATTERN = "js/*";
-    /** Pattern relative to templates base used to match XML templates. */
-    public static final String XML_TEMPLATES_RESOLVE_PATTERN = "xml/*";
-    public static final String TEMPLATES_BASE = "classpath:/templates/";
+	@Autowired
+	private AppRequestInterceptor appRequestInterceptor;
+	/** Pattern relative to templates base used to match text templates. */
+	public static final String TEXT_TEMPLATES_RESOLVE_PATTERN = "text/*";
+	/** Pattern relative to templates base used to match JSON templates. */
+	public static final String JSON_TEMPLATES_RESOLVE_PATTERN = "json/*";
+	/** Pattern relative to templates base used to match JSON templates. */
+	public static final String JS_TEMPLATES_RESOLVE_PATTERN = "js/*";
+	/** Pattern relative to templates base used to match XML templates. */
+	public static final String XML_TEMPLATES_RESOLVE_PATTERN = "xml/*";
+	public static final String TEMPLATES_BASE = "classpath:/templates/";
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-	registry.addInterceptor(appRequestInterceptor);
-    }
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(appRequestInterceptor);
+	}
 
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-	registry.addConverterFactory(new StringToEnumIgnoringCaseConverterFactory());
-	registry.addConverter(new StringToTimePeriod());
-    }
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverterFactory(new StringToEnumIgnoringCaseConverterFactory());
+		registry.addConverter(stringToTimePeriodConverter());
+	}
+
+	@Bean
+	public Converter<String, TimeUtils.TimePeriod> stringToTimePeriodConverter() {
+		return new StringToTimePeriod();
+	}
 
 }
