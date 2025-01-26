4f28771d17bdf4719f9dbee1285ea98c077ba754
Priyanshu Gupta <priyanshugupta@Priyanshus-MacBook-Air.local>
2025-01-26 17:59:59 +0530

    "changes related to json view."

--- bootjx-common-linq/src/main/java/com/boot/jx/sso/View.java
+++ bootjx-common-linq/src/main/java/com/boot/jx/sso/View.java
@@ -1,8 +1,11 @@
 package com.boot.jx.sso;
 
 public class View {
+	public static class External{
+		
+	}
 
-	public static class Public {
+	public static class Public extends External {
 	}
 
 	public static class Internal extends Public {
