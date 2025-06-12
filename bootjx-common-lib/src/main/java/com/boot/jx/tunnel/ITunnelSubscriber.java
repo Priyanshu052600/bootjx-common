aa2db405b31dd4d6e9dee2d1402fc83019ccb2a7
lalit.tanwar <lalit.tanwar07@gmail.com>
2025-06-12 14:44:13 +0530

    Added Reciever for for Send

--- bootjx-common-lib/src/main/java/com/boot/jx/tunnel/ITunnelSubscriber.java
+++ bootjx-common-lib/src/main/java/com/boot/jx/tunnel/ITunnelSubscriber.java
@@ -19,7 +19,7 @@ public interface ITunnelSubscriber<M> {
 		// This is only for help
 	}
 
-	default public void onPush(M message) {
+	default public void onRecieve(M message) {
 		System.out.println("poll:Nothing to " + JsonUtil.toJson(message));
 	}
 
