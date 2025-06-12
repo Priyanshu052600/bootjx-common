d1abdb23fcedad440ced4afe36ad6f55b17f7848
lalit.tanwar <lalit.tanwar07@gmail.com>
2025-06-12 14:45:56 +0530

    onReceive

--- bootjx-common-lib/src/main/java/com/boot/jx/tunnel/ITunnelSubscriber.java
+++ bootjx-common-lib/src/main/java/com/boot/jx/tunnel/ITunnelSubscriber.java
@@ -19,7 +19,7 @@ public interface ITunnelSubscriber<M> {
 		// This is only for help
 	}
 
-	default public void onRecieve(M message) {
+	default public void onReceive(M message) {
 		System.out.println("poll:Nothing to " + JsonUtil.toJson(message));
 	}
 
